fbc570869ccc3b9c128d0f48d1ba69c4bbbb6cd2
kashar0 <kashar@outlook.com>
2026-06-18 21:56:37 +0500

    v1.0.3: API endpoint first and recommended by default in server dropdown

--- custom_components/growatt_cloud/const.py
+++ custom_components/growatt_cloud/const.py
@@ -3,10 +3,10 @@ DEFAULT_POLL_INTERVAL = 5  # minutes
 MIN_POLL_INTERVAL = 2
 
 # Growatt regional server URLs
-DEFAULT_SERVER = "https://server.growatt.com/"
+DEFAULT_SERVER = "https://server-api.growatt.com/"
 SERVERS = {
-    "https://server.growatt.com/":       "Global (server.growatt.com)",
-    "https://eu.growattserver.com/":     "Europe (eu.growattserver.com)",
-    "https://openapi-cn.growatt.com/":   "China (openapi-cn.growatt.com)",
-    "https://server-api.growatt.com/":   "API endpoint (server-api.growatt.com)",
+    "https://server-api.growatt.com/":   "API endpoint - server-api.growatt.com (Recommended)",
+    "https://server.growatt.com/":       "Global - server.growatt.com",
+    "https://eu.growattserver.com/":     "Europe - eu.growattserver.com",
+    "https://openapi-cn.growatt.com/":   "China - openapi-cn.growatt.com",
 }
